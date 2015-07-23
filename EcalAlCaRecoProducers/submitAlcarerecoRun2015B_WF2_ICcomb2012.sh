be0e5bc564c0b663949dc073b988194e3cf6cad5
Louie <louie.dartmoor.corpe@cern.ch>
2015-07-23 13:28:10 +0200

    Run2015B_WF2_ICcomb2015

---
+++ EcalAlCaRecoProducers/submitAlcarerecoRun2015B_WF2_ICcomb2012.sh
@@ -0,0 +1,2 @@
+#Submitting rereco Run2015B_WF2_ICcomb2012.py
+./scripts/RerecoQuick.sh -t config/reRecoTags/Run2015B_WF2_ICcomb2012.py -p RUN2015B  --json_name 51022-251562-Prompt-pfgEcal-noLaserProblem --json /afs/cern.ch/cms/CAF/CMSALCA/ALCA_ECALCALIB/RunII-IC/Run2015B_WF2/251022-251562-Prompt-pfgEcal-noLaserProblem.json --check
